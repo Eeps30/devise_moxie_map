d50df5ecfa1a6223f0a3464b412616c8dfc5c4b3
Evan Ippolito <eeps30@gmail.com>
2018-09-18 14:14:49 -0700

    got map showing up with sidebar

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    createView();
 });
+
+function createView () {
+    new storeLocator.View(map, dataFeed);
+    var features = new storeLocator.FeatureSet(feature1, feature2, feature3);
+    new storeLocator.View(map, dataFeed, {
+    markerIcon: 'icon.png',
+    features: features,
+    geolocation: false
+    });
+    // refresh stores every 10 seconds, regardless of interaction on the map.
+    var view = new storeLocator.View(map, dataFeed, {
+    updateOnPan: false
+    });
+    setTimeout(function() {
+    view.refreshView();
+    }, 10000);
+    // custom MarkerOptions, by overriding the createMarker method.
+    view.createMarker = function(store) {
+    return new google.maps.Marker({
+        position: store.getLocation(),
+        icon: store.getDetails().icon,
+        title: store.getDetails().title
+    });
+    };
+}
